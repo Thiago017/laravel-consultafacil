df0a4a85a652b36ac9b46f35018812298e51351a
Thiago Pereira <thiago.pereira7ps@gmail.com>
2024-07-04 16:51:17 -0300

    Configure LoginRequest

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|unique:users',
+            'password' => 'required'
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'A email is required',
+            'password.required' => 'A password is required',
+        ];
+    }
+}
